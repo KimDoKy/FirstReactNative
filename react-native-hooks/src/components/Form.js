4f74cc5a758f4819bd977239a9c9845691134cef
KimDoKy <makingfunk0@gmail.com>
2021-03-14 18:20:16 +0900

    chap6. Hooks 6.3 useRef

--- react-native-hooks/src/components/Form.js
+++ react-native-hooks/src/components/Form.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import styled from 'styled-components/native';
 
 const StyledTextInput = styled.TextInput.attrs({
@@ -20,9 +20,12 @@ const StyledText = styled.Text`
 const Form = () => {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
+    const refName = useRef(null);
+    const refEmail = useRef(null);
 
     useEffect(() => {
         console.log('\n===== Form Component Mount =====\n');
+        refName.current.focus();
         return () => console.log('\n===== Form Component Unmount =====\n');
     }, []);
 
@@ -34,11 +37,16 @@ const Form = () => {
                 value={name}
                 onChangeText={text => setName(text)}
                 placeholder="name"
+                ref={refName}
+                returnKeyType="next"
+                onSubmitEditing={() => refEmail.current.focus()}
             />
             <StyledTextInput
                 value={email}
                 onChangeText={text => setEmail(text)}
                 placeholder="email"
+                ref={refEmail}
+                returnKeyType="done"
             />
         </>
     );

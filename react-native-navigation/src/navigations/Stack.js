3982e991911305960e2e06c0ea89a22e794e3b2f
KimDoKy <makingfunk0@gmail.com>
2021-03-16 17:24:36 +0900

    chap8. 내비게이션 8.2.3 화면 배경색 수정하기

--- react-native-navigation/src/navigations/Stack.js
+++ react-native-navigation/src/navigations/Stack.js
@@ -8,7 +8,10 @@ const Stack = createStackNavigator();
 
 const StackNavigation = () => {
     return (
-        <Stack.Navigator initialRouteName="Home">
+        <Stack.Navigator 
+            initialRouteName="Home"
+            screenOptions={{ cardStyle: { backgroundColor: '#ffffff' } }}
+        >
             <Stack.Screen name="Home" component={Home} />
             <Stack.Screen name="List" component={List} />
             <Stack.Screen name="Item" component={Item} />
